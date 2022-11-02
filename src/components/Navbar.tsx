3b0d979465c1a0542b69adb319766ca482a0f518
carr-o-t <pandeystakshi@gmail.com>
2022-11-02 22:05:00 +0530

    Fix: responsiveness

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,13 +10,15 @@ import {
     PlusCircleIcon
 } from '@heroicons/react/outline'
 import { HomeIcon as HomeFilledIcon, ChatIcon as ChatFilledIcon } from '@heroicons/react/solid'
-import { Menu, Popover } from '@headlessui/react'
+import { Menu } from '@headlessui/react'
 import { ReactComponent as InstaNavLogo } from '../assets/svgs/insta_logo.svg'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
-import { Insta } from '../types'
 import CreatePost from './CreatePost'
 import { auth, firebaseAuth, fireStore, store } from '../firebase'
 import { useAuth } from '../context/AuthProvider'
+import Search from './Search'
+
+
 
 function Navbar() {
 
@@ -24,15 +26,16 @@ function Navbar() {
     const navigate = useNavigate()
     const [isOpen, setIsOpen] = useState(false)
     const [isCreate, setIsCreate] = useState(false)
-    const [userRef, setUserRef] =  useState<fireStore.DocumentData | undefined>()
+
+    const [userRef, setUserRef] = useState<fireStore.DocumentData | undefined>()
     const { pathname } = useLocation()
     const HomeMenuIcon = pathname === '/' ? HomeFilledIcon : HomeIcon
     const ChatMenuIcon = pathname === '/chat' ? ChatFilledIcon : ChatIcon
 
     const navIcons = [
-        { id: HomeMenuIcon, icon: HomeMenuIcon, onClick: () => { navigate('/')},},
-        { id: ChatMenuIcon, icon: ChatMenuIcon, onClick: () => { } },
-        { id: 'PlusCircleIcon', icon: PlusCircleIcon, onClick: () => { setIsCreate(true)} },
+        { id: HomeMenuIcon, icon: HomeMenuIcon, onClick: () => { navigate('/') }, },
+        { id: ChatMenuIcon, icon: ChatMenuIcon, onClick: () => { navigate('/chat') } },
+        { id: 'PlusCircleIcon', icon: PlusCircleIcon, onClick: () => { setIsCreate(true) } },
         { id: 'HeartIcon', icon: HeartIcon, onClick: () => { } },
     ]
 
@@ -44,43 +47,39 @@ function Navbar() {
     ]
 
     const getCurrentUser = React.useCallback(async () => {
-        await fireStore.getDoc(fireStore.doc(store, `users/${currentUser?.uid}`)).then((querySnapShot) => {
-            if (querySnapShot.exists())
-                setUserRef(querySnapShot.data())
+        await fireStore.onSnapshot(fireStore.doc(store, `users/${currentUser?.uid}`), (querySnapShot) => {
+            setUserRef(querySnapShot.data())
         })
     }, [])
 
     React.useEffect(() => {
         getCurrentUser();
-        
+
     }, [])
 
     return (
         <>
-            <nav className="w-full sticky top-0 h-16 bg-white text-black p-4 border-b border-gray-400">
-                <div className="max-w-5xl flex flex-row justify-between items-center mx-auto">
-                    <Link to="/" className="cursor-pointer">
+            <nav className="w-full z-[2] sticky top-0 h-[fit-content] bg-festa-two p-2 ">
+                <div className="max-w-5xl flex flex-row justify-between items-center mx-auto gap-4">
+                    <Link to="/" className="cursor-pointer sm:basis-[128px]">
                         <div className="">
-                            <InstaNavLogo className="h-8  w-auto" />
-                        </div></Link>
-                    <div className="w-64 min-w-[125px] sm:block hidden">
-                        <input type="text" placeholder='Search' className=" w-full " />
-                    </div>
+                            <InstaNavLogo className="h-6 sm:h-8 w-auto text-festa-one" />
+                        </div>
+                    </Link>
+                    <Search />
                     <div className="flex flex-row justify-between gap-4 items-center">
                         {
-                            navIcons.map(({ id, icon: Component, onClick}) => {
+                            navIcons.map(({ id, icon: Component, onClick }) => {
                                 return (
-                                    // <Link to={to}>
-                                    <Component className='h-7 w-7 cursor-pointer ' onClick={(e) => onClick()} />
-                                    // </Link>
+                                    <Component className='h-7 w-7 cursor-pointer text-festa-one hidden sm:block' onClick={(e) => onClick()} />
                                 )
                             })
                         }
                         <Menu as="div" className="relative h-7 w-7 cursor-pointer">
-                            <Menu.Button className="absolute inset-0 h-7 w-7 rounded-full bg-gray-200 cursor-pointer"
+                            <Menu.Button className="absolute inset-0 h-7 w-7 rounded-full bg-festa-one cursor-pointer"
                                 onClick={() => setIsOpen(!isOpen)}
                             >
-                                <img src={userRef?.photoURL} className="h-full w-full object-cover rounded-full" alt="" />
+                                <img src={userRef?.photoURL} loading="lazy" className="h-full w-full object-cover rounded-full" alt="" />
                             </Menu.Button>
                             <DropDown />
                         </Menu>
@@ -88,28 +87,24 @@ function Navbar() {
                 </div>
             </nav>
 
-            <CreatePost isCreate={isCreate} onClose={(e) => {setIsCreate(false)}} />
+            <CreatePost isCreate={isCreate} onClose={(e) => { setIsCreate(false) }} />
         </>
     )
 }
-
-
 
 function DropDown() {
 
     const navigate = useNavigate()
     const { currentUser } = useAuth()
-    const closePopUp = React.useRef() as React.MutableRefObject<HTMLAnchorElement>;
     const handleLogout = () => {
-        firebaseAuth.signOut(auth).then(() => navigate('/login'))      
+        firebaseAuth.signOut(auth).then(() => navigate('/login'))
     }
-    // if(!isOpen) return <></>
     return (
-        <Menu.Items className="absolute z-10 py-1 px-3 right-0 top-12 bg-white rounded-md w-52 shadow-custom">
+        <Menu.Items className="absolute z-10 py-1 px-3 right-0 top-12 bg-festa-one rounded-md w-52 shadow-custom text-black">
             <Menu.Item>
                 {({ active }) => (
                     <a
-                        className={`flex flex-row items-center gap-5 p-2 mb-1 rounded-md transition-all ease ${active && 'bg-blue-200 text-black font-semibold scale-105 transition-all ease'}`}
+                        className={`flex flex-row items-center gap-5 p-2 mb-1 rounded-md  font-medium ${active && 'bg-festa-six text-festa-one duration-75 font-semibold scale-105 '}`}
                         href={`/profile/${currentUser?.uid}`}
                     >
                         <UserCircleIcon className='h-5 w-5' />
@@ -120,29 +115,40 @@ function DropDown() {
             <Menu.Item>
                 {({ active }) => (
                     <a
-                        className={`flex flex-row items-center gap-5 p-2 mb-1 rounded-md transition-all ease ${active && 'bg-blue-200 text-black font-semibold scale-105 transition-all ease'}`}
-                        href="/saved"
-                    >
-                        <BookmarkIcon className='h-5 w-5' />
-                        <span className="text-sm">Saved</span>
-                    </a>
-                )}
-            </Menu.Item>
-            <Menu.Item>
-                {({ active }) => (
-                    <a
-                        className={`flex flex-row items-center gap-5 p-2 mb-1 rounded-md transition-all ease ${active && 'bg-blue-200 text-black font-semibold scale-105 transition-all ease'}`}
-                        href="/settings"
+                        className={`flex flex-row items-center gap-5 p-2 mb-1 rounded-md  font-medium ${active && 'bg-festa-six text-festa-one duration-75 font-semibold scale-105 '}`}
+                        href="/profile/edit"
                     >
                         <CogIcon className='h-5 w-5' />
-                        <span className="text-sm">Settings</span>
+                        <span className="text-sm">Edit Profile</span>
                     </a>
                 )}
             </Menu.Item>
             <Menu.Item>
                 {({ active }) => (
                     <a
-                        className={`flex flex-row items-center gap-5 p-2 mb-1 rounded-md transition-all ease ${active && 'bg-blue-200 text-black font-semibold scale-105 transition-all ease'}`}
+                        className={`flex sm:hidden flex-row items-center gap-5 p-2 mb-1 rounded-md  font-medium ${active && 'bg-festa-six text-festa-one duration-75 font-semibold scale-105 '}`}
+                        href="/settings"
+                    >
+                        <PlusCircleIcon className='h-5 w-5' />
+                        <span className="text-sm">Create Post</span>
+                    </a>
+                )}
+            </Menu.Item>
+            <Menu.Item>
+                {({ active }) => (
+                    <a
+                        className={`flex sm:hidden flex-row items-center gap-5 p-2 mb-1 rounded-md  font-medium ${active && 'bg-festa-six text-festa-one duration-75 font-semibold scale-105 '}`}
+                        href="/settings"
+                    >
+                        <ChatIcon className='h-5 w-5' />
+                        <span className="text-sm">Chat</span>
+                    </a>
+                )}
+            </Menu.Item>
+            <Menu.Item>
+                {({ active }) => (
+                    <a
+                        className={`flex flex-row items-center gap-5 p-2 mb-1 rounded-md  font-medium ${active && 'bg-festa-six text-festa-one duration-75 font-semibold scale-105 '}`}
                         href="/login"
                         onClick={(e) => handleLogout()}
                     >
@@ -151,7 +157,6 @@ function DropDown() {
                     </a>
                 )}
             </Menu.Item>
-
             <img src="/solutions.jpg" alt="" />
         </Menu.Items>
     )
