aaa2c8fbfcb3f5787870623f255243d992560b3b
carr-o-t <pandeystakshi@gmail.com>
2022-11-03 19:09:34 +0530

    Feat: minor css changes

--- src/components/ProfileBody.tsx
+++ src/components/ProfileBody.tsx
@@ -6,119 +6,74 @@ import {
 import ViewPostModal from './ViewPostModal'
 import { Insta } from '../types'
 import { fireStore, store } from '../firebase'
-import { getUserByID } from '../functions/services'
+import { getPostsByUserID, getUserByID } from '../functions/services'
+import Spinner from './Spinner'
 
 function ProfileBody({ userID }: Insta.ProfileBodyProps) {
 
     const [postCount, setPostCount] = useState(0)
-    const [likeCount, setLikeCount] = useState(0)
-    const [postID, setPostID] = useState<string | undefined>("")
+    const [userDoc, setUserDoc] = useState<fireStore.DocumentData | undefined>();
     const [postList, setPostList] = useState<fireStore.DocumentData | undefined>([])
+    const [userRef, setUserRef] = useState<fireStore.DocumentData | undefined>()
+    const [loading, setLoading] = useState<boolean>(true)
     const [viewPost, setViewPost] = useState({
         view: false,
         postDetail: {},
         id: ''
     })
-    const obj = {
-        likes: 0,
-        comments: 0
-    }
-    const likeRef = React.useRef<number>();
-    const postidRef = React.useRef() as React.MutableRefObject<any>;
 
     React.useEffect(() => {
+        fireStore.onSnapshot(fireStore.query(fireStore.collection(store, 'users'), fireStore.where('uid', "==", userID)), (querySnapShot) => {
+            setUserDoc(querySnapShot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        });
+    }, [])
+
+    React.useEffect(() => {
+        setLoading(true)
         const postRef = fireStore.query(fireStore.collection(store, `posts`), fireStore.where('byUser', "==", userID), fireStore.orderBy("timestamp", "desc"))
         fireStore.onSnapshot(postRef, (querySnapShot) => {
             setPostCount(querySnapShot.size)
-            setPostList([])
-            setPostList(querySnapShot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-            // querySnapShot.docs.map((doc) => {
-            //     setPostList({ ...doc.data(), id: doc.id });
-            //     setPostCount((prev) => prev + 1);
-            // })
         });
-        console.log(postList, "posts")
+        getPostsByUserID(userID).then((result) => setPostList(result)).finally(() => setLoading(false))
     }, [])
 
-    // like count
-     function getLikeCount(postID: string | undefined) {
-        const postRef =  fireStore.collection(store, `posts/${postID}/likedBy`)
-        fireStore.onSnapshot(postRef, (querySnapShot) => {
-            setLikeCount(querySnapShot.size)
-        })
-        return likeCount
-     }
-    
-    React.useEffect(() => {
-        postidRef.current = postID;
-        setPostID(postidRef.current)
-        console.log(postidRef, postID)
-    }, [postID])
-    
-    function getLikes(post: any) {
-        setPostID(post.id)
-        let likes = getLikeCount(post.id);
-        setViewPost({
-            view: true, id:post?.id, postDetail: { ...post }
-        })
-        console.log(likes)
-    }
-    
-    React.useEffect(() => {
-        console.log("likecount altered")
-        likeRef.current = likeCount;
-        setViewPost({
-            ...viewPost, postDetail: { ...viewPost.postDetail, likes: likeRef.current }
-        })
-        console.log(viewPost)
-    }, [likeCount])
 
+    if (loading) return <Spinner />
     return (
         <>
-            <div className='border-t border-t-gray-300  smmd:hidden'>
-                <div className="flex gap-4 p-3 items-center justify-around ">
-                    <div className="justify-center text-sm flex flex-col items-baseline flex-wrap">
-                        <span className="font-semibold w-full text-center m-0 p-0">{postCount}</span>
-                        <span className="text-gray-500 m-0 p-0 text-xs">posts</span>
-                    </div>
-                    <div className="justify-center text-sm flex flex-col items-baseline flex-wrap">
-                        <span className="font-semibold w-full text-center">10</span>
-                        <span className="text-gray-500 text-xs">followers</span>
-                    </div>
-                    <div className="justify-center text-sm flex flex-col items-baseline flex-wrap">
-                        <span className="font-semibold w-full text-center">10</span>
-                        <span className="text-gray-500 text-xs">following</span>
-                    </div>
-                </div>
-            </div>
-            <div className="grid grid-cols-3 gap-1 smmd:gap-6">
-                {/* <p>{userSnapDocRef.current?.uid}</p> */}
+
+            <div className="grid px-6 grid-cols-2 smmd:grid-cols-3 gap-6 smmd:gap-6">
                 {
-                    postList?.map((post: any, idx: number) => {
-                        return (
-                            <div
-                                key={idx}
-                                className="group w-full aspect-square flex justify-center items-center bg-black relative overflow-hidden"
-                                onClick={(e) => getLikes(post)}
-                            >
-                                {
-                                    post.mediaType === "image" ? <img src={post.imgURL} alt="" className="w-auto h-auto object-cover smmd:h-auto" />
-                                        :
-                                        <video src={post.imgURL} className='absolute to-0 right-0 left--0 bottom-0 w-full h-full'></video>
-                                }
-                                <div className="absolute top-0 left-0 w-full h-0 group-hover:cursor-pointer bg-black/50 opacity-0 group-hover:h-full group-hover:opacity-100 transition-all ease-linear duration-300 flex flex-col gap-2 justify-center items-center">
-                                    <div className="flex gap-4 items-center text-white ">
-                                        <HeartIcon className="h-7 w-7" />
-                                        {likeRef.current}
-                                    </div>
-                                    <div className="flex gap-4 items-center text-white ">
-                                        <ChatIcon className="h-7 w-7" />
-                                        10
+                    (postList?.length === 0 && !loading) ? <h1 className="">No Posts!</h1> :
+                        postList?.map((post: any, idx: number) => {
+                            return (
+                                <div
+                                    key={idx}
+                                    className="group w-full aspect-square flex justify-center items-center bg-black relative overflow-hidden"
+                                    onClick={(e) =>
+                                        setViewPost({
+                                            view: true, id: post.id, postDetail: { ...post }
+                                        })
+                                    }
+                                >
+                                    {
+                                        post.mediaType === "image" ? <img src={post.imgURL} loading="lazy" alt="" className="w-auto h-auto object-cover smmd:h-auto" />
+                                            :
+                                            <video src={post.imgURL} className='absolute to-0 right-0 left--0 bottom-0 w-full h-full'></video>
+                                    }
+                                    <div className="absolute top-0 left-0 w-full h-0 group-hover:cursor-pointer bg-black/50 opacity-0 group-hover:h-full group-hover:opacity-100 transition-all ease-linear duration-300 flex flex-col gap-2 justify-center items-center">
+                                        <div className="flex gap-4 items-center text-white ">
+                                            <HeartIcon className="h-7 w-7" />
+                                            {post.likes}
+                                        </div>
+                                        <div className="flex gap-4 items-center text-white ">
+                                            <ChatIcon className="h-7 w-7" />
+                                            0
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        )
-                    })
+                            )
+                        })
                 }
             </div>
             <ViewPostModal postID={viewPost.id} userID={userID} posts={{ ...viewPost }} viewPost={viewPost.view} onClose={(e) => setViewPost({ ...viewPost, view: false })} />
