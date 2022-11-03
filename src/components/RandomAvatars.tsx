1350432ca3c01099959d251922f13d7c7b4a792f
carr-o-t <pandeystakshi@gmail.com>
2022-11-03 19:14:58 +0530

    Feat: random avatars for newly registered users

---
+++ src/components/RandomAvatars.tsx
@@ -0,0 +1,17 @@
+// import rkive from '../assets/default_avatars/rkive.jpeg'
+// import jn from '../assets/default_avatars/jn.jpeg'
+// import yg from '../assets/default_avatars/sua.jpeg'
+// import hope from '../assets/default_avatars/hope.jpeg'
+// import jm from '../assets/default_avatars/jm.jpeg'
+// import thv from '../assets/default_avatars/thv.jpeg'
+// import jk from '../assets/default_avatars/jk.jpeg'
+
+export const RandomAvatar = [
+    "https://firebasestorage.googleapis.com/v0/b/ig-clone-8d433.appspot.com/o/images%2Fjm.jpeg?alt=media&token=83d81e55-d056-4f2f-b121-c8cbc103d42b",
+    "https://firebasestorage.googleapis.com/v0/b/ig-clone-8d433.appspot.com/o/images%2Frkive.jpeg?alt=media&token=76c3e12c-2623-4ded-b74d-af3b600b8e8c",
+    "https://firebasestorage.googleapis.com/v0/b/ig-clone-8d433.appspot.com/o/images%2Fthv.jpeg?alt=media&token=91e48dd5-7a5d-4c08-91b5-f5217e4ee59d",
+    'https://firebasestorage.googleapis.com/v0/b/ig-clone-8d433.appspot.com/o/images%2Fjk.jpeg?alt=media&token=da0ea952-a90b-410d-81de-2c538bf9a5d7',
+    'https://firebasestorage.googleapis.com/v0/b/ig-clone-8d433.appspot.com/o/images%2Fsua.jpeg?alt=media&token=0b20a4ca-0e4a-4652-88a0-fdef39a44ccf',
+    'https://firebasestorage.googleapis.com/v0/b/ig-clone-8d433.appspot.com/o/images%2Fhope.jpeg?alt=media&token=f56bbf41-d4fe-4b12-b665-bd5122e66023',
+    'https://firebasestorage.googleapis.com/v0/b/ig-clone-8d433.appspot.com/o/images%2Fjn.jpeg?alt=media&token=b25abe30-c6b6-4e33-a06a-e48ee7942c6b'
+]
